65b65d95663dca0b4122ca98703a52910f682984
phase8 <ventigo@wp.pl>
2020-02-02 14:43:53 +0100

    next project - changes added

--- 3.1 Projekt3-zadanie2-start.zip/main.js
+++ 3.1 Projekt3-zadanie2-start.zip/main.js
@@ -2,46 +2,58 @@ let size = 10;
 let orderElement = 1;
 
 const init = () => {
-    // tutaj kod
     const btn = document.createElement('button');
     document.body.appendChild(btn);
     const ulElement = document.createElement('ul');
     document.body.appendChild(ulElement);
     btn.innerText = "Dodaj 10 elementów listy";
+    document.addEventListener("click", createLiElements)
 }
 
 const createLiElements = () => {
-
     for (x = 0; x < 10; x++) {
-        var board = document.createElement('li');
-        let ulMain = document.querySelector("ul")
-        ulMain.appendChild(board);
 
+        //creation of element
+        var liElement = document.createElement('li');
+        let ulMain = document.querySelector("ul")
+        liElement.innerText = "Element nr " + orderElement++;
+        liElement.style.listStyle = "none";
+        liElement.style.fontSize = size++ + "px";
+
+        //  append element
+        ulMain.appendChild(liElement);
+        // remember to always create before append !
     }
 
+}
+
+/*
+LONGER AND WORSE ALTERNATIVE:
+const createLiElements = () => {
+    for (x = 0; x < 10; x++) {
+        var liElement = document.createElement('li');
+        let ulMain = document.querySelector("ul")
+        ulMain.appendChild(liElement);
+    }
     let liMain = document.querySelectorAll("li")
-    //let ten = liMain.slice(1, 2)
-
     for (x = 0; x < liMain.length; x++) {
-
         const divyArray = Array.from(liMain)
         let ten = divyArray.slice(-10)
-        console.log(ten)
+      
         ten[x].innerText = "Element nr " + orderElement;
         ten[x].style.listStyle = "none";
         ten[x].style.fontSize = size + "px";
         orderElement++
-        size += 1
+        size++ // u can also write: size += 1
     }
-
-
-
-
-
-    // tutaj kod
 }
+*/
 
-document.addEventListener("click", createLiElements)
+
+
+
+
+
 
 
 
