0d2934a3d03c40bde0ae291c32fe315923e77800
phase8 <ventigo@wp.pl>
2020-07-24 17:15:00 +0200

    changes

---
+++ 5. Projekt 3 Trening pętli for i metody forEach/5 Zadanie 1 pojawienie się elementu i powiększanie go przez kliknięcie przycisku/main.js
@@ -0,0 +1,40 @@
+// tutaj rozwiązanie
+
+
+let el = document.querySelectorAll("li");
+let list = document.querySelector("ul");
+
+
+
+const btn = document.querySelector("button");
+let size = 5
+
+
+function changeHeight() {
+
+    for (let i = 0; i < el.length; i++) {
+        el[i].style.display = "block";
+        el[i].style.fontSize = size + "px"
+    }
+    size += 10
+}
+/* alternative:
+function changeHeight2() {
+
+    el.forEach(each);
+    size += 10
+}
+
+function each(item, index) {
+    item.style.display = "block"
+    item.style.fontSize = size + "px"
+}
+*/
+
+
+
+
+
+
+
+btn.addEventListener("click", changeHeight) // alternative: btn.onclick = changeHeight
