56de091c89b867aa794e62171c245d12e677a5ef
phase8 <ventigo@wp.pl>
2020-08-21 18:46:23 +0200

    changes

--- 13. Projekt 11 - zegar i odliczanie do końca świata/3.1 Projekt11-ZadanieDlaCiebie-Stoper-start.zip/main.js
+++ 13. Projekt 11 - zegar i odliczanie do końca świata/3.1 Projekt11-ZadanieDlaCiebie-Stoper-start.zip/main.js
@@ -1,49 +1,46 @@
  // http://websamuraj.pl/examples/js/projekt11/
 
- var matches = document.getElementsByTagName("p");
+ let startTime = ""
+ let second = 0
+ let millisecond = 0
+ let nowTime = ""
 
- let playStop = document.getElementById("submit")
- let stop = document.getElementById("testo")
-
- //let playStop = document.getElementsByClassName("main")[0]
- //let stop = document.querySelector(".reset")
+ let playStop = document.getElementsByClassName("main")[0]
+ let stop = document.querySelector(".reset")
+ let show = document.querySelector(".show")
  let isActive = false
 
 
 
- const h1 = document.querySelector('h1');
- let number = 0
+ let timerActivator2 = setInterval(timerMilliseconds, 1)
+ clearInterval(timerActivator2)
 
- //function showTime() {}
+ function timerMilliseconds() {
+     nowTime = new Date().getTime();
+     seconds = Math.floor((nowTime - startTime) / 1000)
+     milisecond = Math.floor((nowTime - startTime) % 60);
+     let showMiliseconds = milisecond > 9 ? milisecond : " " + milisecond
+     show.textContent = seconds + "." + showMiliseconds
+ }
 
  playStop.onclick = start
 
  function start() {
-     console.log("start")
-     if (isActive = false) {
+     if (isActive === false) {
          playStop.textContent = "Pauza"
-
-         function timer() {
-             h1.textContent = number
-             number++
-             //stop.onclick = stopFunction;
-             /*
-                     function stopFunction() {
-   
-                         clearInterval(timerActivator)
-                     }
-            */
-         }
-
-         let timerActivator = setInterval(timer, 1000)
+         timerActivator2 = setInterval(timerMilliseconds, 40)
+         startTime = new Date().getTime();
          isActive = !isActive
      } else {
          playStop.textContent = "Start"
-         stopFunction()
+         clearInterval(timerActivator2)
          isActive = !isActive
      }
  }
 
- function stopFunction() {
-     clearInterval(timerActivator)
- }
+
+ /* 
+  // http://websamuraj.pl/examples/js/projekt11/
+
+
+ */
