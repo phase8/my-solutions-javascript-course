e1b6045a68668349f24203c741d6873435ed7e03
phase8 <ventigo@wp.pl>
2020-08-24 19:30:46 +0200

    changes

--- 13. Projekt 11 - zegar i odliczanie do końca świata/3.1 Projekt11-ZadanieDlaCiebie-Stoper-start.zip/main.js
+++ 13. Projekt 11 - zegar i odliczanie do końca świata/3.1 Projekt11-ZadanieDlaCiebie-Stoper-start.zip/main.js
@@ -1,30 +1,68 @@
  // http://websamuraj.pl/examples/js/projekt11/
 
- let test = 0
- let startTime = test + new Date().getTime(); // TUTAJ SKONCZYLES
+ let time = 0
+ let playStop = document.getElementsByClassName("main")[0]
+ let stop = document.querySelector(".reset")
+ let show = document.querySelector(".show")
+ let isActive = false
+ let timerActivator
+
+ function timerMilliseconds() {
+     time++
+     show.textContent = (time / 100).toFixed(2); //  "toFixed" converts a number into a string, rounding the number to keep only two decimals:
+ }
+
+ const stopFunction = () => {
+     time = 0;
+     show.textContent = "---"
+     isActive = !isActive
+     clearInterval(timerActivator)
+ }
+
+ function start() {
+
+     if (!isActive) {
+         playStop.textContent = "Pauza"
+         timerActivator = setInterval(timerMilliseconds, 50)
+         isActive = !isActive
+     } else {
+         playStop.textContent = "Start"
+         clearInterval(timerActivator)
+         isActive = !isActive
+     }
+ }
+
+ playStop.addEventListener('click', start);
+ stop.onclick = stopFunction
+
+ /*
+
+UNFINISHED SOLUTION:
+
+ let startTime = 0
  let second
  let millisecond
  let nowTime
  let stopPeriod = 0
+ let timeWhenStop = 0
+ let timeWhenStart = 0
+ let begin = 0
  let playStop = document.getElementsByClassName("main")[0]
  let stop = document.querySelector(".reset")
  let show = document.querySelector(".show")
  let isActive = false
  let timerActivator = setInterval(timerMilliseconds, 40)
  clearInterval(timerActivator)
- let stopPeriodCount = setInterval(stopPeriod1, 1)
- clearInterval(stopPeriodCount)
 
- function stopPeriod1() {
-     stopPeriod++
-     // console.log(stopPeriod)
- }
 
  function timerMilliseconds() {
      nowTime = new Date().getTime();
-     seconds = Math.floor((nowTime - startTime + stopPeriod) / 1000)
-     milisecond = Math.floor((nowTime - startTime) % 60);
-     let showSeconds = nowTime < 1000 ? 0 : seconds
+     let stopTimePeriod = timeWhenStart - timeWhenStop;
+     //console.log(stopTimePeriod)
+     seconds = Math.floor(((((nowTime) - (startTime + stopTimePeriod)) / 1000)))
+     console.log(startTime)
+     milisecond = Math.floor((nowTime - startTime) % 1000);
+     let showSeconds = startTime < 1000 ? 0 : seconds
      let showMiliseconds = milisecond > 9 ? milisecond : " " + milisecond
      show.textContent = showSeconds + "." + showMiliseconds
  }
@@ -32,19 +70,26 @@
  playStop.onclick = start
 
  function start() {
+
      if (isActive === false) {
-         playStop.textContent = "Pauza"
-         timerActivator = setInterval(timerMilliseconds, 40)
-         clearInterval(stopPeriodCount)
-
-
+         timeWhenStart = new Date().getTime();
+         startTime = startTime == 0 ? new Date().getTime() : startTime
+         new Date().getTime();
+         begin =
+             playStop.textContent = "Pauza"
+         timerActivator = setInterval(timerMilliseconds, 50)
+         //console.log(timeWhenStart)
 
          isActive = !isActive
      } else {
+         timeWhenStop = new Date().getTime();
          playStop.textContent = "Start"
-         stopPeriodCount = setInterval(stopPeriod1, 1)
+
          clearInterval(timerActivator)
          isActive = !isActive
+         //console.log(timeWhenStop)
+
 
      }
  }
+ */
